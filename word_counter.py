11f9effc2a3798443b238affad185f791a0a49fc
Steve Sanchez <1steven.nathan.sanchez@gmail.com>
2025-04-15 15:40:59 -0600

    add: agregar funcion para contar el numero de ocurrencias de una palabra

--- word_counter.py
+++ word_counter.py
@@ -5,3 +5,19 @@ def preprocess_text(text):
         text = text.replace(forbidden_chars[index], " ")
         index += 1
     return text
+
+
+def count_word_occurrences(word, text):
+    word = word.lower()
+    text = preprocess_text(text.lower())
+    words = text.split(" ")
+
+    count = 0
+    index = 0
+
+    while index < len(words):
+        if words[index] != "":
+            if words[index] == word:
+                count += 1
+        index += 1
+    return f"{count} ocurrencias encontradas."
